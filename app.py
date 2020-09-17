936648c5feed195ff1ef6056323636d53d14d893
clemiblac <clemisraha@gmail.com>
2020-09-17 17:18:01 -0400

    flask app added

---
+++ app.py
@@ -0,0 +1,24 @@
+
+from flask import (
+    Flask,
+    render_template,
+    )
+
+is_heroku = False
+if 'IS_HEROKU' in os.environ:
+    is_heroku = True
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+    
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
